e819af8c8fdfe49187be0e2342bf01ebbcd8b520
rasdan <rasdan@kth.se>
2024-02-19 09:36:58 +0100

    feat: add ad-hoc branch-cover tracking
    add global variable in tests/__init__.py `covering` used to
    manually set branch cover status.
    
    add function in __init__.py to run at exit which prints the
    proportion of covered branches.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,21 @@
 from pathlib import Path
+from atexit import register 
 
 FIXTURES = Path(__file__).parent / "fixtures"
+
+# Here you add your function, initializing all the branches to False
+covering = {}
+# example: The function 'export' has 24 branches 
+covering["export"] = [False for _ in range(24)]
+
+# This is called at the end, and pretty-prints the branch coverage 
+def count_covering():
+    for func in covering:
+        print(func + ":", end='\n\t')
+        s = 0
+        for i, e in enumerate(covering[func]):
+            print(f"{i}: {e}", end="\n\t")
+            s += (1 if e else 0)
+        print(f"total coverage: {s / len(covering[func])}")
+
+register(count_covering)
