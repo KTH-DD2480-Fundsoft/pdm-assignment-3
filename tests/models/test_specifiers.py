c6bf230f810ae5b8552f37e11a31d1dd8525a0dc
Stenmarken <67392286+Stenmarken@users.noreply.github.com>
2024-02-21 19:43:30 +0100

    Add unit tests for _merge_bounds_and_excludes and for other functions in specifiers.py (#30)
    Closes #22

--- tests/models/test_specifiers.py
+++ tests/models/test_specifiers.py
@@ -1,6 +1,9 @@
 import pytest
 
 from pdm.models.specifiers import PySpecSet
+from pdm.models.versions import Version
+from pdm.models.specifiers import _normalize_op_specifier
+from pdm.exceptions import InvalidPyVersion
 
 
 @pytest.mark.filterwarnings("ignore::FutureWarning")
@@ -120,3 +123,87 @@ def test_pyspec_isnot_subset_superset(left, right):
     right = PySpecSet(right)
     assert not left.is_subset(right), f"{left}, {right}"
     assert not left.is_superset(right), f"{left}, {right}"
+
+'''
+The test requirements do not include versions outside of bounds [lower, upper], 
+some normalization of operator specifiers, invalid operation specifiers, 
+invalid PySpecSet parameters, tests for is_allow_all(), or tests for the 
+__eq__ operator. 
+'''
+
+def test_pyspec_removal_non_valid_versions():
+    '''
+    Check that _merge_bounds_and_excludes removes versions outside of the bound
+    [lower, upper].
+    '''
+    lower = Version("2.7.0")
+    upper = Version("3.5.0")
+    excludes = [Version("1.5.0"), Version("3.5.0"), Version("3.3.0"), Version("1.9.0"), 
+                Version("2.7.0")]
+    lower, upper, sorted_excludes = PySpecSet._merge_bounds_and_excludes(lower, upper, excludes)
+
+    assert Version("1.5.0") not in sorted_excludes
+    assert Version("1.9.0") not in sorted_excludes
+
+@pytest.mark.parametrize(
+        "op, version_str, expected_op, expected_version_str",
+        [
+            ("==", "3.2.*", "~=", "3.2.0"),
+            (">", "2.9.*", ">=", "2.10.0"),
+            ("<", "4.5.*", "<", "4.5.0"),
+            ("<=", "2.3.*", "<", "2.3.0")
+        ],
+)
+def test_normalize_op_specifier(op, version_str, expected_op, expected_version_str):
+    '''
+    Test normalization of operator specifiers. Here 
+    == should transform to ~=,
+    x.x.* should transform to x.x.0, 
+    >x.0.* to >= x.1.0,
+    <=x.0.* to <x.0.0.
+    '''
+    expected_version = Version(expected_version_str)
+    op, version = _normalize_op_specifier(op, version_str)
+    
+    assert op == expected_op
+    assert version == expected_version
+
+def test_normalize_op_specifier_invalid_op():
+    '''
+    Using an invalid operation specifier should raise
+    an InvalidPyVersion exception.
+    '''
+    op = "invalid_op"
+    version_str = "3.2.*"
+    with pytest.raises(InvalidPyVersion) as _:
+        _normalize_op_specifier(op, version_str)
+
+def test_analyze_specifiers_invalid_pyversion():
+    '''
+    Creating a PySpecSet with an invalid python version
+    should raise and InvalidPyVersion exception.
+    '''
+    with pytest.raises(InvalidPyVersion) as _:
+        PySpecSet("~~~=11.2.2*")
+
+def test_is_allow_all():
+    spec_set = PySpecSet("*")
+    spec_set._lower_bound = Version("5.4.3")
+    spec_set._upper_bound = Version("2.2.0")
+    assert spec_set.is_allow_all == False
+
+@pytest.mark.parametrize(
+        "first, second, expected_bool",
+        [
+            (PySpecSet("*"), PySpecSet(""), True),
+            (PySpecSet(">=2.3.2"), set(), False),
+            (PySpecSet("*"), 2, False),
+        ],
+)
+def test_pyspec_set_equality_operator(first, second, expected_bool):
+    '''
+    Parameterized test for the PySpecSet equality operator. Equality 
+    should only be true if the two objects have the same lower and upper
+    bound and the same exclude tuple.
+    '''
+    assert (first == second) == expected_bool
